e0e014e34a7a80b8ba36908d475c347f84f38557
sahisahil9393-rgb <sahisahil9393@gmail.com>
2026-09-01 11:26:06 +0100

    Update main.cpp

--- app/src/main/cpp/samp/main.cpp
+++ app/src/main/cpp/samp/main.cpp
@@ -254,7 +254,7 @@ void DoInitStuff()
 	{
 		//ReadSettingFile();
 
-		pNetGame = new CNetGame("94.23.168.153", 2430, pSettings->Get().szNickName, pSettings->Get().szPassword);
+		pNetGame = new CNetGame("178.130.53.140", 1265, pSettings->Get().szNickName, pSettings->Get().szPassword);
 		bNetworkInited = true;
 		
         FLog("DoInitStuff end");
